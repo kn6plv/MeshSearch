7a143e3cf152c0750dc7a8b9f6b430641878d94a
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-07-07 17:05:47 -0700

    Start all the pieces

---
+++ Run.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+const Config = require('./config/engine');
+const ChildProcess = require('child_process');
+
+ChildProcess.spawn(`${__dirname}/MeiliSearch/target/release/meilisearch`,
+  [
+    `--http-addr=${Config.address}`,
+    `--master-key=${Config.masterKey}`,
+    `--db-path=${__dirname}/${Config.db}`
+  ],
+  {
+    stdio: 'inherit'
+  }
+);
+
+require('./search/Main');
+require('./crawl/Main');
