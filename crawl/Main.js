f3c17df83ced9df4847a9ad72b897ce3a4c91b6f
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-07-08 20:26:21 -0700

    Fix re-crawl

--- crawl/Main.js
+++ crawl/Main.js
@@ -22,47 +22,46 @@ const lookup = (hostname, _, callback) => {
   });
 }
 
-const q = new UrlQ();
-
-let running = 0;
-q.on('add', async _ => {
-  if (running < Config.threads) {
-    running++;
-    try {
-      for (;;) {
-        const url = q.getNextUrl();
-        if (!url) {
-          break;
-        }
-        console.log(url, q.status());
-        const page = new HttpPage({ url: url, dns: lookup });
-        if (await page.getStatus() === 200) {
-          if (SearchSelector.includePageLinks(page)) {
-            const urls = page.getLinks().links;
-            urls.forEach(url => {
-              if (SearchSelector.includeUrl(url)) {
-                q.addURL(`${url.origin}${url.pathname}${url.search}`);
-              }
-            });
+function crawl() {
+  const q = new UrlQ();
+  let running = 0;
+  q.on('add', async _ => {
+    if (running < Config.threads) {
+      running++;
+      try {
+        for (;;) {
+          const url = q.getNextUrl();
+          if (!url) {
+            break;
           }
-          q.completeUrl(url);
-          if (SearchSelector.includePageInSearch(page)) {
-            u.addDoc(page);
+          console.log(url, q.status());
+          const page = new HttpPage({ url: url, dns: lookup });
+          if (await page.getStatus() === 200) {
+            if (SearchSelector.includePageLinks(page)) {
+              const urls = page.getLinks().links;
+              urls.forEach(url => {
+                if (SearchSelector.includeUrl(url)) {
+                  q.addURL(`${url.origin}${url.pathname}${url.search}`);
+                }
+              });
+            }
+            q.completeUrl(url);
+            if (SearchSelector.includePageInSearch(page)) {
+              u.addDoc(page);
+            }
           }
-        }
-        else {
-          q.completeUrl(url, 'error');
+          else {
+            q.completeUrl(url, 'error');
+          }
         }
       }
+      finally {
+        running--;
+      }
     }
-    finally {
-      running--;
-    }
-  }
-});
-
-function crawl() {
+  });
   q.addURL('http://localnode.local.mesh');
 }
+
 setInterval(crawl, Config.crawlFrequency * 1000);
 crawl();
