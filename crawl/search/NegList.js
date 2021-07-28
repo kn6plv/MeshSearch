982f74eac90a7721aa94aae3bb5a1f5e2313ee64
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-07-28 10:42:56 -0700

    Remove exclusion

--- crawl/search/NegList.js
+++ crawl/search/NegList.js
@@ -15,11 +15,6 @@ module.exports = {
     },
     php: function(url) {
       return url.pathname.toLowerCase().endsWith('.php');
-    },
-
-    // This is causing me trouble so avoid for the moment until I fix it properly
-    DSLRcam: function(url) {
-      return url.pathname.toLowerCase().indexOf('dslrcam') !== -1;
     }
 
   },
