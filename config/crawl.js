22fcbc32d685d93ba5a2e194c6b1e0f879696e53
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-07-31 23:47:08 -0700

    Change default dns

--- config/crawl.js
+++ config/crawl.js
@@ -1,7 +1,7 @@
 const Engine = require('./engine');
 
 module.exports = {
-  dns: [ '10.153.127.81' ],
+  dns: [ '10.50.254.161' ],
   engineHost: `http://${Engine.address}`,
   key: Engine.privateKey,
   index: 'web1',
